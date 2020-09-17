5ef06a62e46e447b9c70aaa0afc41d47b840d47f
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-09-17 10:10:08 +0000

    Fix tolerance and add comparison results

--- ode/bruss/radau5/main.go
+++ ode/bruss/radau5/main.go
@@ -8,14 +8,15 @@ import (
 	"gosl/io"
 	"gosl/mpi"
 	"gosl/ode"
+	"math"
 )
 
 func main() {
 
 	// constants
-	N := 6
-	ndim := 2 * N
-	xend := 10.0
+	N := 20
+	tolExponent := 5
+	tol := math.Pow(0.1, float64(tolExponent))
 
 	// start mpi
 	mpi.Start()
@@ -38,17 +39,21 @@ func main() {
 	// configurations
 	conf := ode.NewConfig("radau5", "", comm)
 	conf.SetStepOut(true, nil)
-	conf.SetTol(1e-2)
+	conf.SetTol(tol)
+	conf.IniH = 1e-6
 
 	// solver
+	ndim := 2 * N
 	sol := ode.NewSolver(ndim, conf, fcn, jac, nil)
 
 	// solve
-	sol.Solve(y, 0, xend)
+	sol.Solve(y, 0.0, 10.0)
 
 	// output root
 	if mpi.WorldRank() == 0 {
-		io.WriteTableVD("results", "yend.txt", []string{"y"}, y)
+		fn := io.Sf("n%d_gosl_tolM%d.txt", N, tolExponent)
+		io.WriteTableVD("results", fn, []string{"y"}, y)
+		io.Pf("tolerance                 = %v\n", tol)
 		sol.Stat.Print(true)
 	}
 }
