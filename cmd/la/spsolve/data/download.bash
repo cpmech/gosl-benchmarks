9cfa369e9cb649bb99797a87bed03b62923fd70f
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-10-02 00:27:56 +0000

    Improve download.bash

--- cmd/la/spsolve/data/download.bash
+++ cmd/la/spsolve/data/download.bash
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-#wget https://suitesparse-collection-website.herokuapp.com/MM/GHS_psdef/inline_1.tar.gz
-tar xzf inline_1.tar.gz
-mv inline_1/inline_1.mtx .
-rmdir inline_1
+get_matrix() {
+  GROUP=$1
+  NAME=$2
+  wget https://suitesparse-collection-website.herokuapp.com/MM/$GROUP/$NAME.tar.gz
+  tar xzf $NAME.tar.gz
+  mv $NAME/$NAME.mtx .
+  rmdir $NAME
+  rm $NAME.tar.gz
+}
+
+get_matrix GHS_psdef inline_1
