8f44bd903dc3af2e5baa05afa8376a4534e64425
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-12-01 05:13:43 +0000

    Add results

---
+++ cmd/la/spsolve/z-umfpack-run-all.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# fnkey = "atmosmodl" // fail
+# fnkey = "Hamrle3" // fail
+
+FNKEYS="\
+  bfwb62 \
+  inline_1 \
+  audikw_1 \
+  Flan_1565 \
+  tmt_unsym \
+  pre2 \
+"
+
+BIN="/tmp/spsolve-umfpack"
+
+for fnkey in $FNKEYS; do
+  echo "... running $fnkey"
+  go build -o $BIN && $BIN -kind="umfpack" -fnkey=$fnkey
+done
